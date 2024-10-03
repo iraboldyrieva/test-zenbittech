757429ec841e54c2649b1c5facd46f9452b6ab36
iraboldyrieva <iraboldyrieva@gmail.com>
2024-10-03 15:32:21 +0300

    Fixed server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import dealRoutes from "./routes/deal.js";
 
@@ -10,6 +11,15 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
+
+// Catch-all route to serve index.html for client-side routing
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api", dealRoutes);
