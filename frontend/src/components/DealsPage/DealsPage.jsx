18301e5b53bd466d1a53252cce8d74ae2fb895a8
iraboldyrieva <iraboldyrieva@gmail.com>
2024-10-03 14:17:36 +0300

    Fixed DealsPage

--- frontend/src/components/DealsPage/DealsPage.jsx
+++ frontend/src/components/DealsPage/DealsPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchDeals } from "../../features/deals/dealsSlice";
-import "./Dealspage.css";
+import "./DealsPage.css";
 
 function DealsPage() {
   const dispatch = useDispatch();
